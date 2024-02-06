0a24bbf9f36b1b5fb8543a89b5edf1369f5d06b4
emanpepars <emanpepars2@gmail.com>
2024-02-06 15:34:51 +0200

    add cache_helper

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,29 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_solution_challenge_2024/features/settings%20/presentation/manager/language_cubit.dart';
-import 'package:google_solution_challenge_2024/config/routes.dart';
-import 'package:google_solution_challenge_2024/core/utils/app_themes.dart';
 
 import 'app.dart';
+import 'core/utils/cache_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await CacheHelper.init();
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ar'), Locale('uk')],
 
       /// --- file lang path ---- ///
       path: 'assets/translations',
-      fallbackLocale: const Locale('en'),
+      fallbackLocale: Locale(CacheHelper.getData("lang") == "Arabic"
+          ? 'ar'
+          : CacheHelper.getData("lang") == "Ukrainian"
+              ? 'uk'
+              : 'en'),
       child: BlocProvider(
         create: (context) => LanguageCubit(),
         child: const PalestineApp(),
       ),
     ),
   );
-// =======
-// class _PalestineAppState extends State<PalestineApp> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ZaitonLightTheme,
-//       debugShowCheckedModeBanner: false,
-//       initialRoute: "/",
-//       onGenerateRoute: (settings) => AppRoutes.onGenerate(settings),
-//     );
-//   } >>>>>>> main
 }
