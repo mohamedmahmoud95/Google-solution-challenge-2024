3a008823a81ac9549852cf568da9027221b916ab
Mohamed Raslan <mohamedraslan@Mohameds-MacBook-Pro.local>
2024-02-13 17:52:01 +0200

    added a landing screen

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -4,48 +4,54 @@ import 'package:google_solution_challenge_2024/features/articles_details/present
 import 'package:google_solution_challenge_2024/features/home/presentation/pages/home_screen.dart';
 import 'package:google_solution_challenge_2024/features/lost%20and%20found/presentation/pages/lost_and_found_screen.dart';
 import 'package:google_solution_challenge_2024/features/mental_health_support/presentation/pages/mental_health_support_screen.dart';
-
-
+import '../features/Landing screen/presentation/pages/landing_screen.dart';
 import '../features/Sign In/presentation/pages/sign_in_screen.dart';
-
 import '../features/settings/presentation/pages/setting_screen.dart';
 
 
 class Routes {
-  static const String home = "/";
+  static const String landing = "/";
+  static const String home = "home";
   static const String settings = "settings";
   static const String articlesDetails = "articlesDetails";
   static const String signIn = "signIn";
   static const String signUp = "signUp";
   static const String mentalHealthSupport = "mentalHealthSupport";
   static const String lostAndFound = "lostAndFound";
-
 }
 
 class AppRoutes {
   static Route onGenerate(RouteSettings routeSettings) {
     switch (routeSettings.name) {
+      case Routes.landing:
+        return MaterialPageRoute(builder: (context) => const LandingScreen());
+
+      case Routes.signIn:
+        return MaterialPageRoute(builder: (context) => const SignInScreen());
+
+      case Routes.signUp:
+        return MaterialPageRoute(builder: (context) => const SignUpScreen());
+
       case Routes.home:
         return MaterialPageRoute(builder: (context) => const HomeScreen());
+
       case Routes.settings:
         return MaterialPageRoute(builder: (context) =>  const Settings());
     
       case Routes.articlesDetails:
         return MaterialPageRoute(builder: (context) => const ArticlesScreen());
-      case Routes.signIn:
-        return MaterialPageRoute(builder: (context) => const SignInScreen());
-      case Routes.signUp:
-        return MaterialPageRoute(builder: (context) => const SignUpScreen());
+
       case Routes.mentalHealthSupport:
         return MaterialPageRoute(
             builder: (context) => const MentalHealthSupportScreen());
+
       case Routes.lostAndFound:
         return MaterialPageRoute(
             builder: (context) => const LostAndFoundScreen());
 
       default:
         return MaterialPageRoute(
-            builder: (context) => const Center(child: Text("")));
+            builder: (context) => const Center(child: Text("Please close the app and re-open it")));
     }
   }
 }
