41f6e941bf30ed2397e7b6c5dacb26c049cbee04
unknown <amrshamsonk@gmail.com>
2024-02-14 19:04:23 +0200

    integrated the state management with ui until mr aboud open the api then i will test

---
+++ lib/features/lost and found/presentation/pages/lost_tab.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_solution_challenge_2024/features/lost%20and%20found/presentation/manager/scan_lost_or_found_person_cubit/scan_lost_or_found_person_cubit.dart';
+
+import '../../../../core/utils/app_colors.dart';
+import '../widgets/lost_person_id_compact_view.dart';
+import '../widgets/lost_person_id_expanded_view.dart';
+import '../widgets/search_bar.dart';
+
+class LostTab extends StatefulWidget {
+  bool compactMode;
+  LostTab({super.key, required this.compactMode});
+
+  @override
+  State<LostTab> createState() => _LostTabState();
+}
+
+class _LostTabState extends State<LostTab> {
+  @override
+  void initState() {
+    BlocProvider.of<ScanLostOrFoundPersonCubit>(context)
+        .scanForLostOrFoundPerson(image: null, isLostPerson: true);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child:
+            BlocBuilder<ScanLostOrFoundPersonCubit, ScanLostOrFoundPersonState>(
+          builder: (context, state) {
+            if (state is ScanLostOrFoundPersonLoaded) {
+              return Wrap(
+                direction: Axis.horizontal,
+                alignment: WrapAlignment.center,
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  const CustomSearchBar(),
+                  ...state.lostOrFoundPersons
+                      .map(
+                        (person) => widget.compactMode
+                            ? LostPersonIdCompactView(
+                                person: person,
+                              )
+                            : LostPersonIdExpandedView(
+                                person: person,
+                              ),
+                      )
+                      .toList(),
+                ],
+              );
+            } else if (state is ScanLostOrFoundPersonError) {
+              //raslan please put the error ui here :)
+              return Center(
+                child: Text("error"),
+              );
+            } else {
+              //raslan please put the loading ui here :)
+
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColorDark,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: AppColors.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/lost_and_found/presentation/cubit/lost_and_found_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'lost_and_found_state.dart';
-
-class LostAndFoundCubit extends Cubit<LostAndFoundState> {
-  LostAndFoundCubit() : super(LostAndFoundInitial());
-}
