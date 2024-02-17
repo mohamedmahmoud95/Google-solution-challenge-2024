e9ad6c0fbf7516c3b8b4e658c445532c9da682e6
unknown <amrshamsonk@gmail.com>
2024-02-17 08:15:14 +0200

    comit

--- lib/features/lost and found/domain/entities/lost_or_found_person.dart
+++ lib/features/lost and found/domain/entities/lost_or_found_person.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-enum LostOrFound {lost, found} //lost = 0 , found = 1
+enum LostOrFound { lost, found } //lost = 0 , found = 1
 
 class LostOrFoundPerson {
   String? firstName;
