18d4500f70886987cab34044fddf1a8f96d6d609
unknown <amrshamsonk@gmail.com>
2024-02-21 22:42:47 +0200

    on my guarantee

--- lib/features/offline_resource/domain/entities/offline_resource.dart
+++ lib/features/offline_resource/domain/entities/offline_resource.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'step.dart';
+import 'offline_resource_step.dart';
 
 part "offline_resource.g.dart";
 
@@ -19,26 +19,26 @@ class OfflineResource {
   @HiveField(3)
   String generalIntro;
   @HiveField(4)
-  List<OfflineResourceStep> steps;
+  List<OfflineResourceStep> offlineResourceSteps;
   @HiveField(5)
   String summary;
-
   OfflineResource({
     required this.id,
     required this.imageUrl,
     required this.title,
     required this.generalIntro,
-    required this.steps,
+    required this.offlineResourceSteps,
     required this.summary,
   });
+
 
   Map<String, dynamic> toMapForFirebase() {
     return <String, dynamic>{
       'id': id,
-      "imageUrl": imageUrl,
+      'imageUrl': imageUrl,
       'title': title,
       'generalIntro': generalIntro,
-      'steps': steps.map((x) => x.toMapForFirebase()).toList(),
+      'offlineResourceSteps': offlineResourceSteps.map((x) => x.toMapForFirebase()).toList(),
       'summary': summary,
     };
   }
@@ -46,15 +46,38 @@ class OfflineResource {
   factory OfflineResource.fromMapForFirebase(Map<String, dynamic> map) {
     return OfflineResource(
       id: map['id'] as String,
+      imageUrl: map['imageUrl'] as String,
       title: map['title'] as String,
-      imageUrl: map["imageUrl"] as String,
       generalIntro: map['generalIntro'] as String,
-      steps: List<Step>.from(
-        (map['steps'] as List<dynamic>).map<Step>(
-          (x) => Step.fromMapForFirebase(x as Map<String, dynamic>),
-        ),
-      ),
+      offlineResourceSteps: List<OfflineResourceStep>.from((map['offlineResourceSteps'] as List<int>).map<OfflineResourceStep>((x) => OfflineResourceStep.fromMapForFirebase(x as Map<String,dynamic>),),),
       summary: map['summary'] as String,
     );
+  }
+  @override
+  String toString() {
+    return 'OfflineResource(id: $id, imageUrl: $imageUrl, title: $title, generalIntro: $generalIntro, offlineResourceSteps: $offlineResourceSteps, summary: $summary)';
+  }
+
+  @override
+  bool operator ==(covariant OfflineResource other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.imageUrl == imageUrl &&
+      other.title == title &&
+      other.generalIntro == generalIntro &&
+      listEquals(other.offlineResourceSteps, offlineResourceSteps) &&
+      other.summary == summary;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      imageUrl.hashCode ^
+      title.hashCode ^
+      generalIntro.hashCode ^
+      offlineResourceSteps.hashCode ^
+      summary.hashCode;
   }
 }
