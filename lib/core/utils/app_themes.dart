41778f65a00efd6282ae03359304beaf42fc4e4c
Mohamed Raslan <mohamedraslan@Mohameds-MacBook-Pro.local>
2024-02-21 17:09:07 +0200

    added (no match found) error message in lost and found screen

--- lib/core/utils/app_themes.dart
+++ lib/core/utils/app_themes.dart
@@ -7,24 +7,32 @@ ThemeData ZaitonDarkTheme = ThemeData(
   primaryColorLight: const Color(0xFF52796f),
   cardColor: const Color(0xFF84a98c),
   highlightColor: const Color(0xFFcad2c5),
-
 );
-
 
 ThemeData ZaitonLightTheme = ThemeData(
   scaffoldBackgroundColor: AppColors.white,
   cardColor: const Color(0xFF84a98c),
   primaryColor: AppColors.oliveGreen2,
-  progressIndicatorTheme: ProgressIndicatorThemeData(
+  progressIndicatorTheme: const ProgressIndicatorThemeData(
     color: AppColors.oliveGreen2,
   ),
+
   primaryColorLight: const Color(0xFF52796f),
   primaryColorDark: const Color(0xFF354f52),
   highlightColor: const Color(0xFF2f3e46),
-  appBarTheme: AppBarTheme(
+
+  appBarTheme: const AppBarTheme(
+    elevation: 0,
+    surfaceTintColor: AppColors.white,
     backgroundColor: AppColors.white,
+    titleTextStyle: TextStyle(
+      fontSize: 22,
+      overflow: TextOverflow.ellipsis,
+      color: AppColors.black0000,
+    ),
   ),
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
+
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
     backgroundColor: AppColors.oliveGreen2,
-  )
+  ),
 );
