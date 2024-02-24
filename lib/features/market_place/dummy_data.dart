7030ae8f5a3328468c31d31ca7d9873cfac2bf26
Mohamed Raslan <mohamedraslan@Mohameds-MacBook-Pro.local>
2024-02-24 02:00:55 +0200

    UI updates in multiple files

---
+++ lib/features/market_place/dummy_data.dart
@@ -0,0 +1,66 @@
+//================================================================================================================//
+//Categories
+import 'package:google_solution_challenge_2024/core/utils/app_images.dart';
+import 'package:google_solution_challenge_2024/features/market_place/domain/product_category.dart';
+
+ProductCategory clothes = ProductCategory(
+  id: '1',
+  name: "Clothes",
+  imageUrl: AppImages.clothes2,
+);
+
+
+
+ProductCategory nutrition3 = ProductCategory(
+  id: '1',
+  name: "Authentic food",
+  imageUrl: AppImages.food3,
+);
+
+
+ProductCategory appliances = ProductCategory(
+  id: '1',
+  name: "Home",
+  imageUrl: AppImages.appliances,
+);
+
+
+ProductCategory handmade = ProductCategory(
+  id: '1',
+  name: "Handmade",
+  imageUrl: AppImages.crafts,
+);
+
+ProductCategory handmade2 = ProductCategory(
+  id: '1',
+  name: "Handmade",
+  imageUrl: AppImages.handmade,
+);
+
+ProductCategory handmade3 = ProductCategory(
+  id: '1',
+  name: "Handmade",
+  imageUrl: AppImages.handmade2,
+);
+
+
+ProductCategory tools = ProductCategory(
+  id: '1',
+  name: "Tools",
+  imageUrl: AppImages.tools,
+);
+
+
+//========================//
+List<ProductCategory> sampleProductCategories = [
+  clothes,nutrition3,appliances,handmade3,tools
+];
+//================================================================================================================//
+
+
+
+//================================================================================================================//
+//
+
+
+//================================================================================================================//
